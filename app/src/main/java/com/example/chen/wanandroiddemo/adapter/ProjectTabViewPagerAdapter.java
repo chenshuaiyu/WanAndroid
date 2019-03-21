eb5ea6a3fe8ef007d345a02c6db7bf2099a21030
chenshuaiyu <664251867@qq.com>
2019-03-21 20:51:41 +0800

    添加项目栏

---
+++ app/src/main/java/com/example/chen/wanandroiddemo/adapter/ProjectTabViewPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.chen.wanandroiddemo.adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.chen.wanandroiddemo.ui.fragment.ProjectTabFragment;
+
+import java.util.List;
+
+/**
+ * Coder : chenshuaiyu
+ * Time : 2019/3/21 8:44
+ */
+public class ProjectTabViewPagerAdapter extends FragmentStatePagerAdapter {
+    private List<Fragment> mFragments;
+
+    public ProjectTabViewPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        mFragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        ProjectTabFragment fragment = (ProjectTabFragment) mFragments.get(position);
+        return fragment.getProjectTab().getName();
+    }
+}
